f2fa15dd31ace74896f1f9acfc6e662a7e41cec6
Trey Billing <trey@Treys-MacBook-Pro.local>
2022-04-28 09:01:26 -0400

    Initial commit with minimal functions and documentation.

---
+++ R/generate_movers.R
@@ -0,0 +1,50 @@
+#' Generate event counts from ACLED data
+#'
+#' @param data ACLED data.
+#' @param unit_id Unit variable.
+#' @param time_id Temporal variable.
+#' @param slide_funs Requested moving statistics. Character vector with options including mean, median, min, and max.
+#' @param slide_periods How many periods in the past to summarize over. Vector of one or more integers. Inf includes all previous periods.
+#' @param na.rm  Whether to include NAs in the calculations.
+#' @return Returns a tibble grouped by unit_id.
+#'
+#'
+#'
+#' @export
+
+
+generate_movers <-
+  function(data, var, unit_id, time_id, slide_funs, slide_periods, na.rm = T, complete = T) {
+
+    all_funs <- list(mean = function(x) {mean(x, na.rm = na.rm)},
+                     sd = function(x) {sd(x, na.rm = na.rm)},
+                     min = function(x) {min(x, na.rm = na.rm)},
+                     max = function(x) {max(x, na.rm = na.rm)})
+
+    cross_tbl <- expand_grid(all_funs, slide_periods) %>%
+      mutate(fun_name = attr(all_funs, "name")) %>%
+      filter(fun_name %in% slide_funs)
+
+
+    data %>%
+      group_by(.data[[unit_id]]) %>%
+      arrange(.data[[unit_id]], .data[[time_id]]) %>%
+      # mutate(var_lag = case_when(isFALSE(include_present) ~ lag(.data[[var]], 1),
+      #                        TRUE ~ as.numeric(.data[[var]]))) %>%
+
+      nest() %>%
+      mutate(moving = map(data,
+                          function(df) {
+                            map2_dfc(.x = cross_tbl$all_funs,
+                                     .y = cross_tbl$slide_periods,
+                                     ~slider::slide_dbl(.x = df[[var]], .f = .x, .before = .y,
+                                                        .complete = complete)) %>%
+                              rename_with(., ~paste("moving", attr(cross_tbl$all_funs, "name"), cross_tbl$slide_periods, sep = "_"))
+                          }
+      )
+      ) %>%
+
+      unnest(c(data, moving)) %>%
+      suppressMessages()
+
+  }
