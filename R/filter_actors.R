c9e84ccfbb698bf6aaf3a243177ef39d9f6b9214
Rachel Goodman <rachel.goodman37@gmail.com>
2022-09-30 12:36:47 -0700

    adding examples to documentation

--- R/filter_actors.R
+++ R/filter_actors.R
@@ -1,7 +1,7 @@
 #' @title Filter ACLED Data by Actor
 #' @name filter_actors
-#' @description This function allows users to filter ACLED data to a single actor or list of actors.
-#' @param df dataframe. ACLED dataframe to filter.
+#' @description This function allows users to filter ACLED data by actor.
+#' @param df dataframe. ACLED data to filter.
 #' @param actors character vector or vector of character strings. Actor name or list of actor names to use for filtering.
 #' @param filter_cols character string. Columns to use for filtering. Default is "all", which filters based on all actor columns (i.e. actor1, actor2, assoc_actor_1, assoc_actor_2). "Primary" filters based on actor1 and actor2 column only.
 #' @returns Returns a tibble of ACLED data filtered to selected actors.
@@ -9,9 +9,22 @@
 #' @examples
 #' \dontrun{
 #'
+#' # Request events from Yemen
+#' df_events <- acled_api(countries = "Yemen",
+#'                        start_date = "2022-01-01",
+#'                        end_date = "2022-07-30",
+#'                        monadic = F,
+#'                        acled_access = TRUE)
+#'
+#' # Create vector of actors of interest
+#' yemen_actors <- c('Military Forces of Yemen (2016-) Supreme Political Council', 'Military Forces of Yemen (2012-)')
+#'
+#' # Filter to selected actors
+#' filtered_df <- filter_actors(df = df_events, actors = yemen_actors, filter_cols = 'all)
+#'
 #' }
 #' @md
-#' @import httr
+#' @import tidyverse
 #'
 #' @export
 
