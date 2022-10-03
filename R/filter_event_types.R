86a258f6d9f3989466931684c321c3879ed1e880
billingtt <billingtt@gmail.com>
2022-10-03 13:33:30 -0400

    Add "keep_all_events" param

--- R/filter_event_types.R
+++ R/filter_event_types.R
@@ -1,9 +1,11 @@
 #' Filter ACLED data by event type categories
 #'
-#' @param data ACLED data
+#' @param data ACLED event data
 #' @param acled_categories character vector of ACLED event type categories. Options include: political_violence, organized_political_violence, disorder, and demonstrations. More than one category may be requested at one time (e.g., acled_categories = c("organized_political_violence", "disorder")).
+#' @param keep_all_events logical. If FALSE (default), the returned data will include only events within the requested categories. If TRUE, the returned data will retain all events, even those that do not fall within the requested categories. New dummy columns for each requested category denote which categories each event type falls within.
 #' @import dplyr
-#' @return Returns a tibble including only ACLED events that fall within the requested event categories, including new columns (dummy variables) indicating which categories each event falls under.
+#' @return Returns a tibble of ACLED events and new columns (dummy variables) indicating which categories each event falls under. If `keep_all_events = FALSE`, only events that fall within one of the requested categories are returned. If `keep_all_events = TRUE`, all events, including those not within a requested category, are returned.
+#'
 #' @family Data Manipulation
 #' @examples
 #' \dontrun{
@@ -33,7 +35,9 @@
 #'
 #' @export
 
-filter_event_types <- function(data, acled_categories) {
+filter_event_types <- function(data,
+                               acled_categories,
+                               keep_all_events = FALSE) {
 
   if("event_type" %in% colnames(data) == FALSE) {
     stop("Column 'event_type' not in data. Please use data with columns for both ACLED 'event_type' and 'sub_event_type'.")
@@ -48,11 +52,20 @@ filter_event_types <- function(data, acled_categories) {
     stop("Requested 'acled_category' is not a designated option. Valid options include: political_violence, organized_political_violence, disorder, and demonstrations.")
   }
 
-  out_data <- data %>%
-    left_join(acledR::acled_event_categories %>%
-                select(event_type, sub_event_type, acled_categories)) %>%
-    filter(if_any(acled_categories, ~.x > 0)) %>%
-    ungroup()
+  if(isFALSE(keep_all_events)) {
+    out_data <- data %>%
+      left_join(acledR::acled_event_categories %>%
+                  select(event_type, sub_event_type, acled_categories)) %>%
+      filter(if_any(acled_categories, ~.x > 0)) %>%
+      ungroup()
+  }
+  else {
+    out_data <- data %>%
+      left_join(acledR::acled_event_categories %>%
+                  select(event_type, sub_event_type, acled_categories)) %>%
+      ungroup()
+
+  }
 
   return(out_data)
 
