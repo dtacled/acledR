23519e3532bdd7598b1594ee673fda1da467a6d9
billingtt <billingtt@gmail.com>
2022-10-03 09:23:37 -0400

    Add acled_event_categories data

--- R/data.R
+++ R/data.R
@@ -30,3 +30,17 @@
 #' \item{region_name}{Region names}
 #' \item{first_event_date}{First date (yyyy-mm-dd) coded by ACLED}}
 "acled_regions"
+
+#' ACLED Event Categories
+#'
+#' ACLED event and sub-event types, grouped by category
+#'
+#' @format A data frame:
+#' \describe{
+#' \item{event_type}{ACLED event type}
+#' \item{sub_event_type}{ACLED sub-event type}
+#' \item{political_violence}{Dummy indicator for whether sub-event type falls within \textit{political violence}}
+#' \item{organized_political_violence}{Dummy indicator for whether sub-event type falls within \textit{organized political violence}}
+#' \item{disorder}{Dummy indicator for whether sub-event type falls within \textit{disorder}}
+#' \item{demonstrations}{Dummy indicator for whether sub-event type falls within \textit{demonstrations}}
+"acled_event_categories"
