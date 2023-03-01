525a50aa70c4262a9bd87d9de8d2ebc3bcb29299
lfagliano <lucasfagliano@gmail.com>
2023-03-01 12:08:53 +0400

    Add test and log argument to acled_api
    The log argument to the acled_api is used for the testthat test.
    
    Because of the new argument, I have also addressed the vignette

---
+++ tests/testthat/test-acled_api.R
@@ -0,0 +1,19 @@
+# acled_api test
+
+
+test_that("number of columns is correct", {
+  expect_equal(ncol(received_data),31)
+})
+
+test_that("names of columns are correct", {
+  expect_equal(names(received_data),columns)
+})
+
+test_that("Split calls for big calls", {
+  expect_equal(as.numeric(ceiling(sum(log_received_data$time)/300000)),max(log_received_data$calls))
+})
+
+test_that("Error when one of two countries are wrong",{
+          expect_error(acled_api(email = "acledexamples@gmail.com", key = "M3PWwg3DIdhHMuDiilp5",countries = c("Argentia","Bolivia"),
+                                 start_date="2022-01-01",end_date = "2022-12-31",prompt = F, acled_access = F, log = F),
+                       regexp = "One or more of the requested countries *")})
