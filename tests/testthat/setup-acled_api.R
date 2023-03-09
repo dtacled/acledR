18b3d6eaad9fb2cf69be7b786ed68cc6ba3950e3
lfagliano <lucasfagliano@gmail.com>
2023-03-09 14:19:11 +0400

    Edits to test that failed CMD

--- tests/testthat/setup-acled_api.R
+++ tests/testthat/setup-acled_api.R
@@ -4,6 +4,9 @@ received_data <- acled_api(email = "acledexamples@gmail.com", key = "M3PWwg3DIdh
 log_received_data <- acled_api(email = "acledexamples@gmail.com", key = "M3PWwg3DIdhHMuDiilp5",regions = c("Western Africa", "Eastern Africa", "Europe"),end_date = "2022-12-31",prompt = F, acled_access = F, log = T)
 received_data_numeric_region <- acled_api(email = "acledexamples@gmail.com", key = "M3PWwg3DIdhHMuDiilp5",regions = 1,end_date = "2022-12-31",prompt = F, acled_access = F)
 
+is_empty <- acled_api(email = "acledexamples@gmail.com", key = "M3PWwg3DIdhHMuDiilp5", start_date='2021-01-01', acled_access = F,prompt=F)
+
+
 columns <- c("data_id","iso","event_id_cnty","event_id_no_cnty","event_date","year","time_precision",
              "event_type","sub_event_type","actor1","assoc_actor_1","inter1","actor2","assoc_actor_2",
              "inter2","interaction","region","country","admin1","admin2","admin3","location","latitude",
