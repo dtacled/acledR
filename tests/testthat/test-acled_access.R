e552c5b425becafc208b7cf55337412426edae49
lfagliano <lucasfagliano@gmail.com>
2023-06-01 22:04:35 +0400

    Removing misplaced elements in the package structure

--- tests/testthat/test-acled_access.R
+++ tests/testthat/test-acled_access.R
@@ -1,8 +1,15 @@
+
+# Does it save the credentials in the enviornment? - Missing
+
+
+# Shows the message that it was successful?
 test_that("It shows that it works", {
   expect_message(acled_access(email = "acledexamples@gmail.com", key = "M3PWwg3DIdhHMuDiilp5"), "Success! Credentials authorized")
 })
 
-test_that("It shows that is doesn't work", {
-  expect_error(acled_access(email = "acledexamples@gmail123123.com", key = "M3PWwg3DIdhHMuDiilp5"), regex = "Key and email not authorized.*")
-})
 
+# Does it shows a message when it fails?
+test_that("It shows that is doesn't work", {
+  expect_error(acled_access(email = "acledexamples@gmail123123.com", key = "M3PWwg3DIdhHMuDiilp5"),
+               regex = "Key and email not authorized.*")
+})
