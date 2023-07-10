6e95ab8880df614b606b43eb178f072810269652
lfagliano <lucasfagliano@gmail.com>
2023-07-10 12:22:52 +0400

    cyhanges to articles and finish the tests

--- tests/testthat/setup-acled_api.R
+++ tests/testthat/setup-acled_api.R
@@ -13,4 +13,3 @@ columns <- c("event_id_cnty","event_date","year","time_precision","disorder_type
              "event_type","sub_event_type","actor1","assoc_actor_1","inter1","actor2","assoc_actor_2",
              "inter2","interaction","civilian_targeting","iso","region","country","admin1","admin2","admin3","location","latitude",
              "longitude","geo_precision","source","source_scale","notes","fatalities","tags","timestamp")
-
