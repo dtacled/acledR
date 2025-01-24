0eb1795c07a6794d9985776654ce33af35a2b87a
billingtt <billingtt@gmail.com>
2025-01-24 11:51:10 -0500

    Small changes for cran check

--- R/acled_rounding.R
+++ R/acled_rounding.R
@@ -4,6 +4,7 @@
 #' @param num int. This is the number we are trying to round.
 #' @param digits int. Where do we want to round up. It accepts 0 (whole number), 1 (tenth place), 2 (hundredths), etc.
 #' @family Helpers
+#' @returns A rounded numeric value
 #' @details
 #' This function is meant to address the problem of rounding in R where the approach is always round to even. The function is meant to round things following the simple rule. If the decimal is 5+ then round up, if not round down. With the 'digits' argument, one can set up the specificity of the rounding, 0= whole number, 1 = tenth place, 2=hundreds place, and so on.
 #' @examples
